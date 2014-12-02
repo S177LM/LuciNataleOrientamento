01a00e0919acb8e9c9cee3d2652a5dad00d68af4
LRuffati <lruffati@gmail.com>
2014-12-02 17:40:21 +0100

    Importante cambio alla logica della funzione che gestisce il bottone. Ulteriori chiarimenti nel readme

--- CodiceArduino_base/CodiceArduino_base.ino
+++ CodiceArduino_base/CodiceArduino_base.ino
@@ -7,35 +7,39 @@ void setup() {
 void loop() {
   while (1){    //assegnate le `coreografie` dentro questo loop, uno per coreografia. 
     digitalWrite(13,HIGH);
-    if (lstnr(1000, 2)==1) {break;} //questo deve essere usate al posto del delay. Se usate una variabile per il delay questa deve essere di tipo !!!long!!! non int
+    if (lstnr(1000, 2)==1) {goto end1;} //questo deve essere usate al posto del delay. Se usate una variabile per il delay questa deve essere di tipo !!!long!!! non int
+                                        //non copiate e incollate ovunque. Se si deve uscire dal primo ciclo usare end1, dal secondo end2 e così via
     digitalWrite(13,LOW);
     if (lstnr(1000, 2)==1) {break;}
   }
+  end1:;
   Serial.println("Stato 1->2"); //questa è importante per la comunicazione con l'interfaccia desktop
   digitalWrite(13,LOW);         //si assicura che il led sia spento (fatela per ogni led che accendete in quella sopra)
-/*  
+  
   while (1){
     digitalWrite(13,HIGH);
-    if (lstnr(5000, 2)==1) {break;} 
+    if (lstnr(5000, 2)==1) {goto end2;} 
     digitalWrite(13,LOW);
-    if (lstnr(2000, 2)==1) {break;}
+    if (lstnr(2000, 2)==1) {goto end2;}
   }
+  end2:;
   Serial.println("Stato 2->3");
   digitalWrite(13,LOW);
   
   while (1){
     digitalWrite(13,HIGH);
-    if (lstnr(3000, 2)==1) {break;} 
+    if (lstnr(3000, 2)==1) {goto end3;} 
     digitalWrite(13,LOW);
-    if (lstnr(3000, 2)==1) {break;}
+    if (lstnr(3000, 2)==1) {goto end3;}
   }
+  end3:;
   Serial.println("Stato 3->1");
   digitalWrite(13,LOW);
-*/
+  
 }
 
 int lstnr(long dl, int pn_i ){  //Attenzione! Se si usa una variabile per passare il tempo di attesa deve essere di tipo long
-//usare questa funzione così: if (lstnr(1000, 2)==1) {break;}
+//usare questa funzione così: if (lstnr(1000, 2)==1) {goto statement;}
 //1000 è l'attesa, 2 il pin del bottone
   long c_t = millis();
   int a=0;
@@ -45,6 +49,6 @@ int lstnr(long dl, int pn_i ){  //Attenzione! Se si usa una variabile per passar
       break;
     }
   }
-  delay(200); //se non c'è questo delay il bottone è come fosse premuto anche molte volte di fila
+  delay(250); //se non c'è questo delay il bottone è come fosse premuto anche molte volte di fila
   return a;
 }
